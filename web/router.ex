16b60eab1c9fc3593c52f1ee860547d39a79423c
Radek Molenda <rmolenda@leadformance.com>
2016-02-26 16:44:21 +0100

    Add basic auth

--- web/router.ex
+++ web/router.ex
@@ -7,6 +7,7 @@ defmodule BasicAuthExercise.Router do
     plug :fetch_flash
     plug :protect_from_forgery
     plug :put_secure_browser_headers
+    plug BasicAuth, username: "user", password: "secret"
   end
 
   pipeline :api do
