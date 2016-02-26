e768d26aa4e85bfbec0851b79fe415e1e9d0486c
Radek Molenda <radek.molenda@gmail.com>
2016-02-26 18:03:53 +0100

    Failing test

--- test/controllers/page_controller_test.exs
+++ test/controllers/page_controller_test.exs
@@ -6,6 +6,11 @@ defmodule BasicAuthExercise.PageControllerTest do
     |> put_req_header("authorization", "Basic dXNlcjpzZWNyZXQ=")
   end
 
+  def with_invalid_authorization_header(conn) do
+    conn
+    |> put_req_header("authorization", "Basic I like turtles")
+  end
+
   test "GET / without authorization header should throw 401", %{conn: conn} do
     conn = get conn, "/"
     assert response(conn, 401) == "unauthorized"
@@ -16,5 +21,12 @@ defmodule BasicAuthExercise.PageControllerTest do
     |> with_valid_authorization_header()
     |> get("/")
     assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+  end
+
+  test "GET / with incorrect authorization should throw 401", %{conn: conn} do
+    conn = conn
+    |> with_invalid_authorization_header()
+    |> get("/")
+    assert response(conn, 401) == "unauthorized"
   end
 end
